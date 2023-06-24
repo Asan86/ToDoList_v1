1c17036f25f4e85b1c4feff688dd87feaca2bfee
AsanTurdukulov <asanturdukulov86@gmail.com>
2023-06-24 23:11:02 +0600

    ToDoList-v1

---
+++ src/components/Todos/TodoForm.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import styles from "./TodoForm.module.css"
+
+function TodoForm({addTodo}) {
+   const [text, setText] = useState("");
+   const onSubmitHendler = (event) => {
+      event.preventDefault()
+      addTodo(text)
+      setText('')
+   }
+  return (
+   <div className={styles.todoFormContainer}>
+    <form onSubmit={onSubmitHendler}>
+      <input placeholder='Enter new todo' value={text}
+      onChange={(e)=> setText(e.target.value )} />
+      <button type='submit'>Submit</button>
+    </form>
+    </div>
+  )
+}
+
+export default TodoForm
+ 
